c34de078236c53e2cbf05c71bdaac90e3a05c5ec
Marcelo Hidalgo <marcelohidalgop14@gmail.com>
2023-10-25 06:15:10 -0300

    Arreglo btn Servicio

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
                     <h2>Servicios</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
-                    <a href="" class="btn">Start Now</a>
+                    <a href="" class="btn">Comenzar Ahora</a>
                 </div>
                 <div class="accordian box wow slideInRight">
                     <div class="accordian-container active">
